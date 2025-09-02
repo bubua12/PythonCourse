b0528e3f9cb4c33aba92336b0c4ef739da9ad1d5
bubua12 <wangty334752@gmail.com>
2025-09-02 23:11:34 +0800

    Initial commit

---
+++ 04_func.py
@@ -0,0 +1,15 @@
+import math
+
+result = math.sqrt(16)
+
+print(result)
+
+print(math.sqrt(64))
+
+'''
+多行注释
+这是第二行
+
+
+哈哈哈哈哈哈哈哈哈哈
+'''
---
+++ 05_data_type.py
@@ -0,0 +1,16 @@
+# 对字符串求长度
+len("Hello World")
+len("Hello World\n")
+
+# index索引值是从0开始的
+hello = "Hello World"
+print(hello[1])
+
+# print(hello[122])
+
+# NoneType ---> None
+# 返回数据类型 ---> 数据类型重要，反应了可以在这个数据类型上做什么操作
+type(hello)
+
+# len(5)
+# 对错误的类型使用函数
